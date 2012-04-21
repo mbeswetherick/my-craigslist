7a6e62fa683b01b315685d91b860daba48d8000b
Michael Beswetherick <mbeswetherick@gmail.com>
2012-04-21 16:17:58 -0700

    url,date category and both locations

--- my_craigslist.js
+++ my_craigslist.js
@@ -6,8 +6,16 @@ getPageContents();
 var priceArray = [];
 
 function getPageContents(){
+	//date
+	var date = $('.ban');
+	var category = $('.bchead').find('a');
+	date = date.text();
+	console.log(date);
 	$("p").each( function (index) {
 		try {
+			//date again
+			console.log(date);
+			//price
 	 		var price = $(this).text().match(/\$\d+/);
 	   		console.log(price[0]);
 			//links
@@ -18,6 +26,17 @@ function getPageContents(){
 			var title = $(this).find('a');
 			title = title.text();
 			console.log(title);
+			//specific location
+			console.log($(this).find('font').text());
+			//general location
+			var url = $(location).attr('href');
+			url = url.split('.');
+			console.log(url[0].split('/')[2]);
+			//category
+			category = $('a[href$="pta/"]').text();
+			category = category.split('-');
+			category = category[0];
+			console.log(category);
 		} catch (TypeError) {}
 	});
 }
