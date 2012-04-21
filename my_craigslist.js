dd877bd6a47e855787d1414c2d8b15bbf790d18c
Michael Beswetherick <mbeswetherick@gmail.com>
2012-04-21 15:32:17 -0700

    added titles

--- my_craigslist.js
+++ my_craigslist.js
@@ -1,19 +1,23 @@
 
 
-price();
+getPageContents();
 
 
 var priceArray = [];
 
-function price(){
+function getPageContents(){
 	$("p").each( function (index) {
 		try {
 	 		var price = $(this).text().match(/\$\d+/);
 	   		console.log(price[0]);
 			//links
-			anchor = $(this).find('a');
+			var anchor = $(this).find('a');
 			var link = $(anchor).attr('href');
 			console.log($(anchor).attr('href'));
+			//titles
+			var title = $(this).find('a');
+			title = title.text();
+			console.log(title);
 		} catch (TypeError) {}
 	});
 }
