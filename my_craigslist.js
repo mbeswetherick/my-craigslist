7b1c7a7273ef0eb2a92aeb8e6d62baa4332f141d
Michael Beswetherick <mbeswetherick@gmail.com>
2012-04-21 15:20:34 -0700

    added links

--- my_craigslist.js
+++ my_craigslist.js
@@ -6,12 +6,19 @@ price();
 var priceArray = [];
 
 function price(){
-	$("p").each( function () {
+	$("p").each( function (index) {
 		try {
 	 		var price = $(this).text().match(/\$\d+/);
 	   		console.log(price[0]);
+			//links
+			anchor = $(this).find('a');
+			var link = $(anchor).attr('href');
+			console.log($(anchor).attr('href'));
 		} catch (TypeError) {}
 	});
 }
+
+
+
 
 
