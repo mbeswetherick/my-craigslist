5293273b0d99e649224240f3c49893cf7b0c85e9
Michael Beswetherick <mbeswetherick@gmail.com>
2012-04-21 19:08:34 -0700

    fixed the bugs... now onto the graph

--- my_craigslist.js
+++ my_craigslist.js
@@ -14,11 +14,19 @@ sub = split[1]
 
 function getPageContents(){
 	//date
-	var date = $('.ban');
-	date = date.text();
-	date = date.substring(3,date.length); 
-	date = date.trim()
-	console.log(date);
+	url = $(location).attr('href');
+	url = url.split('/');
+	//the dates are formatted differently if there is a search ,'>/
+	if(url[3] == "search"){
+		var date = $(".row").text();
+		date = date.trim();
+		date = date.substring(0,7).trim();
+	} else{
+		var date = $('.ban');
+		date = date.text();
+		date = date.substring(3,date.length); 
+		date = date.trim()
+	}
 	$("p").each( function (index) {
 		try {
 			//initialize post array
@@ -40,6 +48,8 @@ function getPageContents(){
 			//titles
 			var title = $(this).find('a');
 			title = title.text();
+			title= title.split('-');
+			title = title[0].trim();
 			console.log(title);
 			postArray[3] = title;
 			//specific location
@@ -56,8 +66,11 @@ function getPageContents(){
 			category = $(category).text();
 			console.log(category);
 			postArray[6] = category;
+			//query
+			var query = $('input').attr('value');
+			postArray[7] = query;
 			//post
-			$.post("http://postbin.heroku.com/837a36b9", { date: postArray[0], price: postArray[1], url : postArray[2], title : postArray[3], location_specific : postArray[4], location_wide: postArray[5], category: postArray[6] } );
+			$.post("http://postbin.heroku.com/837a36b9", { date: postArray[0], price: postArray[1], url : postArray[2], title : postArray[3], location_specific : postArray[4], location_wide: postArray[5], category: postArray[6], query:postArray[7] } );
 		} catch (TypeError) {}
 	});
 }
